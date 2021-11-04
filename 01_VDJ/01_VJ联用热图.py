f992f1ac9b80efde05af09adf01ce10c8c02c1fa
kay990 <73207032+kay990@users.noreply.github.com>
2021-11-04 18:56:41 +0800

    Update 01_VJ联用热图.py

--- 01_VDJ/01_VJ联用热图.py
+++ 01_VDJ/01_VJ联用热图.py
@@ -16,3 +16,7 @@ df = pd.read_csv('PRJNA330606.tsv', sep='\t', header=0)
 #按VJ分组（groupby）-去重（将重复VJ的clonefraction相加，agg）-将分好的组再合并起来（reset_index）
 hm_data = df.groupby(['Vregion', 'Jregion']).agg({'cloneFraction':sum}).reset_index()
 
+#上一步去过重，但是是对于VJ联合而言的，对于V或者J单个而言还是有重复的，单列去重用Counter（字符串/列表/数组输入，输出字典）
+#字典形式变成dataframe用pd.DataFrame.from_dict，
+V = pd.DataFrame.from_dict(Counter(hm_data.Vregion.values), orient = 'index', columns = ['values']).index.tolist()
+J = pd.DataFrame.from_dict(Counter(hm_data.Jregion.values), orient = 'index', columns = ['values']).index.tolist()
